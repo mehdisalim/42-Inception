9d56f3643822629c0f1135afbb0059c56024ca2a
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-04-29 22:33:28 +0100

    setup mariadb and remove unusefull files.

--- srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+echo "
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';
+FLUSH PRIVILEGES;" > /queries.sql
 
-mariadb < queries.sql
+mariadbd --user=mysql --bootstrap < /queries.sql
+
+exec "$@"
--- srcs/requirements/mariadb/tools/queries.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
-GRANT ALL ON wordpress.* TO 'wordpress'@'localhost';
-FLUSH PRIVILEGES;
