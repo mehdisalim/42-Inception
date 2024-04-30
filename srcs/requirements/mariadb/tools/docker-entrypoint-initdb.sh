36f72f5ff5e767c89d6636966e463de2681908d7
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-04-30 15:57:13 +0100

    finish setuping mariadb container.

--- srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
@@ -5,8 +5,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';
-FLUSH PRIVILEGES;" > /queries.sql
-
-mariadbd --user=mysql --bootstrap < /queries.sql
+FLUSH PRIVILEGES;" | mariadbd --user=mysql --bootstrap
 
 exec "$@"
