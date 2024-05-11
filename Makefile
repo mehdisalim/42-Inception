0096bb5e197a32deb0aa1cabca8f5db7564b695f
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-05-11 16:42:05 +0100

    finish

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
-
-
-start	:
+up: build
 	docker compose -f srcs/docker-compose.yml up -d
 
-stop	:
+build :
+	@mkdir -p ~/data/wordpress_data
+	@mkdir -p ~/data/mariadb_data
+	docker compose -f srcs/docker-compose.yml build
+
+fclean : clean
+	docker system prune -af
+
+clean:
 	docker compose -f srcs/docker-compose.yml down
 
-ils		:
-	docker images ls -a
 
-cls		:
-	docker container ls -a
+clean_wordpress_data :
+	rm -rf ~/data/wordpress_data/*
+	
+clean_mariadb_data :
+	rm -rf ~/data/mariadb_data/*
+
+re: fclean fclean up
--- srcs/requirements/wordpress/tools/docker-entrypoint-initdb.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint-initdb.sh
@@ -7,23 +7,11 @@ then
 
     wp core download --allow-root
 
-    wp config create --allow-root \
-                    --dbname=${MYSQL_DATABASE} \
-                    --dbuser=${MYSQL_USER_NAME} \
-                    --dbpass=${MYSQL_USER_PASSWORD} \
-                    --dbhost=${WORDPRESS_DB_HOST}
+    wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER_NAME} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=${MYSQL_DB_HOST} --prompt=${MYSQL_USER_PASSWORD}
 
-    wp core install --allow-root \
-        --url=${DOMAIN_NAME} \
-        --title=${WORDPRESS_TITLE} \
-        --admin_user=${WORDPRESS_USER_NAME} \
-        --admin_password=${WORDPRESS_USER_PASSWORD} \
-        --admin_email=${USER_EMAIL}
+    wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN_NAME} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}
 
-    wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" \
-        --role=editor \
-        --user_pass=${MYSQL_USER_PASSWORD} \
-        --path=/var/www/html/wordpress --allow-root
+    wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" --role=editor --user_pass=${WORDPRESS_USER_PASSWORD} --path=/var/www/html/wordpress --allow-root
 
     chown -R www-data:www-data /var/www/html/
     chmod -R 777 /var/www/html/
