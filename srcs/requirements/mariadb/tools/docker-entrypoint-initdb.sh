8c859e4cae3f871e2ac8f658f0925883c1278bb6
mehdisalim <mehdisalimofficial2000@gmail.com>
2024-05-12 20:02:35 +0100

    last commit

--- srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint-initdb.sh
@@ -2,17 +2,17 @@
 
 echo "
 FLUSH PRIVILEGES;
-ALTER USER '${MYSQL_ADMIN_NAME}'@'localhost' IDENTIFIED BY '$(< /run/secrets/mysql_root_password)';
+ALTER USER '${MYSQL_ADMIN_NAME}'@'localhost' IDENTIFIED BY '$(cat /run/secrets/mysql_root_password)';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '$(< /run/secrets/mysql_user_password)';
+CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '$(cat /run/secrets/mysql_user_password)';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';
 FLUSH PRIVILEGES;"
 
 echo "
 FLUSH PRIVILEGES;
-ALTER USER '${MYSQL_ADMIN_NAME}'@'localhost' IDENTIFIED BY '$(< /run/secrets/mysql_root_password)';
+ALTER USER '${MYSQL_ADMIN_NAME}'@'localhost' IDENTIFIED BY '$(cat /run/secrets/mysql_root_password)';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '$(< /run/secrets/mysql_user_password)';
+CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '$(cat /run/secrets/mysql_user_password)';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';
 FLUSH PRIVILEGES;" | mariadbd -u mysql --bootstrap
 
--- srcs/requirements/wordpress/tools/docker-entrypoint-initdb.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint-initdb.sh
@@ -7,11 +7,11 @@ then
 
     wp core download --allow-root
 
-    wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER_NAME --dbpass="$(< /run/secrets/mysql_user_password)" --dbhost=$MYSQL_DB_HOST --prompt="$(< /run/secrets/mysql_user_password)"
+    wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER_NAME --dbpass="$(cat /run/secrets/mysql_user_password)" --dbhost=$MYSQL_DB_HOST --prompt="$(cat /run/secrets/mysql_user_password)"
 
-    wp core install --allow-root --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$(< /run/secrets/wordpress_admin_password) --admin_email=$WORDPRESS_ADMIN_EMAIL
+    wp core install --allow-root --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$(cat /run/secrets/wordpress_admin_password) --admin_email=$WORDPRESS_ADMIN_EMAIL
 
-    wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" --role=editor --user_pass=$(< /run/secrets/wordpress_user_password) --path=/var/www/html/wordpress --allow-root
+    wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" --role=editor --user_pass=$(cat /run/secrets/wordpress_user_password) --path=/var/www/html/wordpress --allow-root
 
     chown -R www-data:www-data /var/www/html/
     chmod -R 777 /var/www/html/
